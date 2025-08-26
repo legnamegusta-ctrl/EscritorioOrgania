135c5d1d87b87c4a407e5133bdd3eaf541cf4ecd
legnamegusta-ctrl <legnamegusta@gmail.com>
2025-08-26 14:39:18 -0300

    3

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -4,9 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase
 
 // TODO: replace with your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyDB0l95P05wQcbJhrw5qqvyToihwlVZ-Us",
+  authDomain: "agropecuaria3l-b9bcc.firebaseapp.com",
+  projectId: "agropecuaria3l-b9bcc",
+  storageBucket: "agropecuaria3l-b9bcc.firebasestorage.app",
+  messagingSenderId: "91137815308",
+  appId: "1:91137815308:web:c247c909fb1f793b270f29"
 };
 
 const app = initializeApp(firebaseConfig);
